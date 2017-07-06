f866463e4205879b2fb985fd74f554ee5334fe5e
Aurélien Manteaux <amanteaux@coreoz.com>
2017-07-06 17:28:58 +0200

    IE compatibility

--- plume-demo-full-guice-jersey/admin-ui/app/app.js
+++ plume-demo-full-guice-jersey/admin-ui/app/app.js
@@ -29,6 +29,22 @@ var app = angular.module('admin',[
 		.fallbackLanguage('en')
 	;
 })
+.config(function($httpProvider) {
+	// for IE caching : https://stackoverflow.com/a/19771501/3790208
+	// initialize get if not there
+	if (!$httpProvider.defaults.headers.get) {
+		$httpProvider.defaults.headers.get = {};
+	}
+
+	// Answer edited to include suggestions from comments
+	// because previous version of code introduced browser-related errors
+
+	// disable IE ajax request caching
+	$httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+	// extra
+	$httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+	$httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+})
 .run(function($rootScope, $state, sessionService, $translate) {
 	$rootScope.$on("$stateChangeStart", function(event, toState) {
 		if(!toState.public && !sessionService.isConnected()) {
